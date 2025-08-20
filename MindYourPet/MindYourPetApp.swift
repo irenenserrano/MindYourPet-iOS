895c260c77ad0c4f3bace70b3d5c8ff513fd1abb
Irene Serrano <irenenserrano@gmail.com>
2025-08-20 14:14:37 -0700

    Initial Commit

---
+++ MindYourPet/MindYourPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindYourPetApp.swift
+//  MindYourPet
+//
+//  Created by Irene Serrano on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MindYourPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MindYourPetTests/MindYourPetTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindYourPetTests.swift
+//  MindYourPetTests
+//
+//  Created by Irene Serrano on 8/20/25.
+//
+
+import Testing
+@testable import MindYourPet
+
+struct MindYourPetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
