bee4eca2f4306ab6abcfc5deb3f638f5684334af
Irene Serrano <irenenserrano@gmail.com>
2025-09-16 13:13:15 -0700

    Modifying Alarm section presentation

--- MindYourPet/ProfileDetailView.swift
+++ MindYourPet/ProfileDetailView.swift
@@ -17,7 +17,7 @@ struct ProfileDetailView: View {
     @State private var emptyAlarmsList: Bool = false
     @State var petProfileID: String
     @FocusState private var isEditorFocused: Bool
-    
+    @State var timeString: String = ""
     struct MyReminderItem: Identifiable {
         let id = UUID()
         let title: String
@@ -34,145 +34,162 @@ struct ProfileDetailView: View {
     }
     
     var body: some View {
-        ZStack {
-            let newListOfReminders = listOfReminders.map { reminder in
-                MyReminderItem(
-                    title: reminder["title"] as! String,
-                    reminderTime: reminder["reminderTime"] as! Date,
-                    notes: reminder["notes"] as! String,
-                    repeating: reminder["repeating"] as! Bool
-                )
-            }// end newListOfReminders
-            
-            let newListOfAlarms = listOfAlarms.map { alarm in
-                MyAlarmItem(
-                    label: alarm["label"] as! String,
-                    alarmTime: alarm["alarmTime"] as! Date,
-                    isActive: alarm["isActive"] as! Bool
-                )
-            }// end newListOfReminders
-            
-            List {
-                Section {
-                    TextEditor(text: $note)
-                        .frame(height: 100)
-                        .focused($isEditorFocused)
-                        .onAppear {
-                            Task {
-                                await fetchCareNotes(profileID: petProfileID)
-                            }
-                        }
-                        .toolbar {
-                            ToolbarItemGroup(placement: .keyboard) {
-                                Spacer()
-                                Button("Done") {
-                                    isEditorFocused = false
+        NavigationStack {
+            ZStack {
+                let newListOfReminders = listOfReminders.map { reminder in
+                    MyReminderItem(
+                        title: reminder["title"] as! String,
+                        reminderTime: reminder["reminderTime"] as! Date,
+                        notes: reminder["notes"] as! String,
+                        repeating: reminder["repeating"] as! Bool
+                    )
+                }// end newListOfReminders
+                
+                let newListOfAlarms = listOfAlarms.map { alarm in
+                    MyAlarmItem(
+                        label: alarm["label"] as! String,
+                        alarmTime: alarm["alarmTime"] as! Date,
+                        isActive: alarm["isActive"] as! Bool
+                    )
+                }// end newListOfReminders
+                
+                List {
+                    Section {
+                        TextEditor(text: $note)
+                            .frame(height: 100)
+                            .focused($isEditorFocused)
+                            .onAppear {
+                                Task {
+                                    await fetchCareNotes(profileID: petProfileID)
                                 }
                             }
-                        }
-                } header: {
-                    Text("Care notes")
-                        .font(.title.bold())
-                        .foregroundStyle(.black)
-                }// end Section
-                .listRowBackground(Color.pink.brightness(0.7))
-                .onChange(of: note) { newValue in
-                    Task {
-                        await uploadCareNotes(text: newValue, profileID: petProfileID)
-                    }
-                }
-                
-                
-                Section {
-                    if emptyRemindersList {
-                        Text("Add a reminder...")
-                    }
-                    ForEach(newListOfReminders) { reminder in
-                        VStack{
-                            NavigationLink {
-                                
-                            } label: {
-                                HStack {
-                                    Text(reminder.title)
-                                }// end HStack
+                            .toolbar {
+                                ToolbarItemGroup(placement: .keyboard) {
+                                    Spacer()
+                                    Button("Done") {
+                                        isEditorFocused = false
+                                    }
+                                }
                             }
-                        }
-                    }// end ForEach
-                } header: {
-                    HStack {
-                        Text("Reminders")
+                    } header: {
+                        Text("Care notes")
                             .font(.title.bold())
                             .foregroundStyle(.black)
-                        
-                        Spacer()
-                        
-                        Button {
-                        } label: {
-                            Image(systemName: "plus.circle")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 30)
-                                .foregroundColor(.pink)
-                                .brightness(0.7)
+                    }// end Section
+                    .listRowBackground(Color.pink.brightness(0.7))
+                    .onChange(of: note) { newValue in
+                        Task {
+                            await uploadCareNotes(text: newValue, profileID: petProfileID)
                         }
-                    }// end HStack
-                }// end Section
-                .listRowBackground(Color.pink.brightness(0.7))
-                
-                Section {
-                    if emptyAlarmsList {
-                        Text("Add an alarm...")
                     }
-                    ForEach(newListOfAlarms) { alarm in
-                        VStack{
-                            NavigationLink {
-                                
-                            } label: {
-                                HStack {
-                                    Text(alarm.label)
-                                }// end HStack
-                            }
+                    
+                    Section {
+                        if emptyRemindersList {
+                            Text("Add a reminder...")
                         }
-                    }// end ForEach
-                } header: {
-                    HStack {
-                        Text("Alarms")
-                            .font(.title.bold())
-                            .foregroundStyle(.black)
-                        Spacer()
-                        Button {
+                        ForEach(newListOfReminders) { reminder in
+                            VStack{
+                                NavigationLink {
+                                    
+                                } label: {
+                                    HStack {
+                                        Text(reminder.title)
+                                    }// end HStack
+                                }
+                            }
+                        }// end ForEach
+                    } header: {
+                        HStack {
+                            Text("Reminders")
+                                .font(.title.bold())
+                                .foregroundStyle(.black)
                             
-                        } label: {
-                            Image(systemName: "plus.circle")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 30)
-                                .foregroundColor(.pink)
-                                .brightness(0.7)
+                            Spacer()
+                            
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "plus.circle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 30)
+                                    .foregroundColor(.pink)
+                                    .brightness(0.7)
+                            }// end Button
+                        }// end HStack
+                    }// end Section
+                    .listRowBackground(Color.pink.brightness(0.7))
+                    
+                    Section {
+                        if emptyAlarmsList {
+                            Text("Add an alarm...")
                         }
-                    }// end HStack
-                }// end Section
-                .listRowBackground(Color.pink.brightness(0.7))
-            }// end List
-            .scrollContentBackground(.hidden)
-            .background(Color.pink.ignoresSafeArea().brightness(0.5))
-        }// end ZStack
-        .onAppear {
-            Task {
-                listOfReminders = await loadReminders(profileID: petProfileID)
+                        ForEach(newListOfAlarms) { alarm in
+                            VStack{
+                                NavigationLink {
+                                    
+                                } label: {
+                                    HStack {
+                                        Text(alarm.label)
+                                        Text(timeString)
+                                        Spacer()
+                                        
+                                        if alarm.isActive {
+                                            Text("ON")
+                                        } else {
+                                            Text("OFF")
+                                        }
+                                    }// end HStack
+                                    .onAppear {
+                                        Task {
+                                            let dateformatter = DateFormatter()
+                                            dateformatter.dateFormat = "h:mm a"
+                                            timeString = dateformatter.string(from: alarm.alarmTime)
+                                        }
+                                    }
+                                }// end NavigationLink
+                            }// end VStack
+                        }// end ForEach
+                    } header: {
+                        HStack {
+                            Text("Alarms")
+                                .font(.title.bold())
+                                .foregroundStyle(.black)
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "plus.circle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 30)
+                                    .foregroundColor(.pink)
+                                    .brightness(0.7)
+                            }// end Button
+                        }// end HStack
+                    }// end Section
+                    .listRowBackground(Color.pink.brightness(0.7))
+                }// end List
+                .scrollContentBackground(.hidden)
+                .background(Color.pink.ignoresSafeArea().brightness(0.5))
+            }// end ZStack
+            .onAppear {
+                Task {
+                    listOfReminders = await loadReminders(profileID: petProfileID)
+                    
+                    if listOfReminders.isEmpty {
+                        emptyRemindersList.toggle()
+                    }
+                    
+                    listOfAlarms = await loadAlarms(profileID: petProfileID)
+                    
+                    if listOfAlarms.isEmpty {
+                        emptyAlarmsList.toggle()
+                    }
+                }// end Task
                 
-                if listOfReminders.isEmpty {
-                    emptyRemindersList.toggle()
-                }
-                
-                listOfAlarms = await loadAlarms(profileID: petProfileID)
-                
-                if listOfAlarms.isEmpty {
-                    emptyAlarmsList.toggle()
-                }
-            }// end Task
-            
-        }// end onAppear
+            }// end onAppear
+        }// end NavigationStack
     }// end body
     
     func loadReminders(profileID: String) async -> [[String: Any]]{
